799b854d2295bd9cb3d377414c2617e81277bb33
Hamdija Halilovic <hamdija_hh@hotmail.com>
2020-11-12 16:30:56 +0100

    Challenge 2 in progress...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+lemon = Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+mojito = Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Bloody Mary")
+Cocktail.create(name: "Sex on the Beach")
+Cocktail.create(name: "Margarita")
+Cocktail.create(name: "Pina Colada")
+
+Dose.create(cocktail: mojito, ingredient: lemon, description: "6 leaves")
+Dose.create(description: "1.5 oz")
+Dose.create(description: "4.5 cl")
